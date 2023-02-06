335319c210b44d8629d5b5db5701811bf18e1538
Lara-Vel <lara.vel@outlook.com>
2023-02-07 00:44:22 +0100

    refactorice app function to traverse array and add imports components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Card from "./components/Gentleman/Gentleman";
+import gentlemanData from "./gentleman.data";
+import "./components/Gentleman/Gentleman.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="main-header">
+        <h1 className="main-title">The pointing gentlemen</h1>
       </header>
+      <section className="controls">
+        <p className="info">0 gentlemen pointing at you</p>
+        <button className="button button--select">Select all</button>
+      </section>
+      {gentlemanData.map((gentleman) => (
+        <Card gentleman={gentleman} />
+      ))}
     </div>
   );
 }
